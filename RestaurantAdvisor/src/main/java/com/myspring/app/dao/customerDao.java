420e6d9a7925cb2b2a280f7b3ea82c6666994d69
Andrew Jacobson <andrew.jacobson22@gmail.com>
2017-12-02 11:51:21 -0600

    Add files via upload

---
+++ RestaurantAdvisor/src/main/java/com/myspring/app/dao/customerDao.java
@@ -0,0 +1,45 @@
+package com.myspring.app.dao;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+import com.myspring.app.model.Customer;
+import com.myspring.app.util.ConnectionFactory;
+
+@Service
+public class customerDao {
+	
+	public customerDao() {
+		
+	}
+
+	public List<Customer> checkCustomer(String email) {
+		SqlSession session = ConnectionFactory.getSqlSessionFactory().openSession();
+		List<Customer> c = new ArrayList<Customer>();
+		try {
+			c = session.selectList("com.mapper.CustomerMapper.selectCustomer",email);
+		} finally {
+			session.close();
+		}
+		return c;
+	}
+		
+	public int insertCustomer(Customer c) {
+		SqlSession session = com.myspring.app.util.ConnectionFactory.getSqlSessionFactory().openSession();
+		int result;
+		try {
+			result = session.insert("com.mapper.CustomerMapper.insertCustomer",c);
+			session.commit();
+		} finally {
+			session.close();
+		}
+		return result;		
+	}
+	
+}
---
+++ RestaurantAdvisor/src/main/webapp/WEB-INF/views/mainpage.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Restaurant Advisor System</title>
+</head>
+<body>
+<h2>Welcome to the main page!</h2>
+
+
+
+<h4>To transfer control to a view using a new controller file, click <a href="/app/restaurantList">here</a>.</h4>
+</body>
+</html>
